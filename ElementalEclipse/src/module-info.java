c277e344c463e79d2b7c9385a72fdd42cc4744e6
shunm <shunmugapriya.ramanathan@gmail.com>
2024-12-24 00:36:39 -0600

    ok

--- ElementalEclipse/src/module-info.java
+++ ElementalEclipse/src/module-info.java
@@ -5,11 +5,12 @@
  * 
  */
 module ElementalEclipse {
-//	requires javafx.base;     // Required for basic functionality
+	requires javafx.base;     // Required for basic functionality
     requires javafx.controls; // Required for UI controls like buttons, labels, etc.
 //    requires javafx.fxml;
     requires java.scripting;
 	requires java.desktop;
 	requires javafx.graphics;
     exports pack;
+//    exports ElementalEclipse
 }
